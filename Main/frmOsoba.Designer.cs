7d747639d11de296d30bfef764df0f424554298d
Роман Гульчевський <romkogulcevskij@gmail.com>
2025-05-18 20:59:02 +0300

    lab5

--- Main/frmOsoba.Designer.cs
+++ Main/frmOsoba.Designer.cs
@@ -42,11 +42,32 @@
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.btnFind = new System.Windows.Forms.ToolStripButton();
+            this.btnFilter = new System.Windows.Forms.ToolStripButton();
             this.label1 = new System.Windows.Forms.Label();
             this.txtFind = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpDNOTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpDNOFrom = new System.Windows.Forms.DateTimePicker();
+            this.btnFilterCancel = new System.Windows.Forms.Button();
+            this.btnFilterOk = new System.Windows.Forms.Button();
+            this.cmbSex = new System.Windows.Forms.ComboBox();
+            this.cmbAdres = new System.Windows.Forms.ComboBox();
+            this.txtReitTo = new System.Windows.Forms.TextBox();
+            this.txtReitFrom = new System.Windows.Forms.TextBox();
+            this.txtPIP = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
             this.bindingNavigator1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -54,7 +75,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(36, 47);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(699, 368);
+            this.dataGridView1.Size = new System.Drawing.Size(832, 233);
             this.dataGridView1.TabIndex = 0;
             // 
             // bindingNavigator1
@@ -72,7 +93,8 @@
             this.bindingNavigatorMoveNextItem,
             this.bindingNavigatorMoveLastItem,
             this.bindingNavigatorSeparator2,
-            this.btnFind});
+            this.btnFind,
+            this.btnFilter});
             this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
             this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
             this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
@@ -80,7 +102,7 @@
             this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
             this.bindingNavigator1.Name = "bindingNavigator1";
             this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
-            this.bindingNavigator1.Size = new System.Drawing.Size(800, 25);
+            this.bindingNavigator1.Size = new System.Drawing.Size(886, 25);
             this.bindingNavigator1.TabIndex = 1;
             this.bindingNavigator1.Text = "bindingNavigator1";
             // 
@@ -163,11 +185,21 @@
             this.btnFind.Text = "Find";
             this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
             // 
+            // btnFilter
+            // 
+            this.btnFilter.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnFilter.Image = ((System.Drawing.Image)(resources.GetObject("btnFilter.Image")));
+            this.btnFilter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(23, 22);
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(580, 16);
+            this.label1.Location = new System.Drawing.Point(708, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 16);
             this.label1.TabIndex = 2;
@@ -176,7 +208,7 @@
             // 
             // txtFind
             // 
-            this.txtFind.Location = new System.Drawing.Point(619, 15);
+            this.txtFind.Location = new System.Drawing.Point(756, 15);
             this.txtFind.Name = "txtFind";
             this.txtFind.Size = new System.Drawing.Size(100, 20);
             this.txtFind.TabIndex = 4;
@@ -184,11 +216,202 @@
             this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
             this.txtFind.Leave += new System.EventHandler(this.txtFind_Leave);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpDNOTo);
+            this.groupBox1.Controls.Add(this.dtpDNOFrom);
+            this.groupBox1.Controls.Add(this.btnFilterCancel);
+            this.groupBox1.Controls.Add(this.btnFilterOk);
+            this.groupBox1.Controls.Add(this.cmbSex);
+            this.groupBox1.Controls.Add(this.cmbAdres);
+            this.groupBox1.Controls.Add(this.txtReitTo);
+            this.groupBox1.Controls.Add(this.txtReitFrom);
+            this.groupBox1.Controls.Add(this.txtPIP);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(36, 297);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(832, 160);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            this.groupBox1.Visible = false;
+            this.groupBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBox1_Paint);
+            // 
+            // dtpDNOTo
+            // 
+            this.dtpDNOTo.Location = new System.Drawing.Point(352, 109);
+            this.dtpDNOTo.Name = "dtpDNOTo";
+            this.dtpDNOTo.Size = new System.Drawing.Size(200, 20);
+            this.dtpDNOTo.TabIndex = 17;
+            // 
+            // dtpDNOFrom
+            // 
+            this.dtpDNOFrom.Location = new System.Drawing.Point(352, 58);
+            this.dtpDNOFrom.Name = "dtpDNOFrom";
+            this.dtpDNOFrom.Size = new System.Drawing.Size(200, 20);
+            this.dtpDNOFrom.TabIndex = 16;
+            // 
+            // btnFilterCancel
+            // 
+            this.btnFilterCancel.Location = new System.Drawing.Point(735, 106);
+            this.btnFilterCancel.Name = "btnFilterCancel";
+            this.btnFilterCancel.Size = new System.Drawing.Size(81, 21);
+            this.btnFilterCancel.TabIndex = 15;
+            this.btnFilterCancel.Text = "Відміна";
+            this.btnFilterCancel.UseVisualStyleBackColor = true;
+            this.btnFilterCancel.Click += new System.EventHandler(this.btnFilterCancel_Click);
+            // 
+            // btnFilterOk
+            // 
+            this.btnFilterOk.Location = new System.Drawing.Point(735, 54);
+            this.btnFilterOk.Name = "btnFilterOk";
+            this.btnFilterOk.Size = new System.Drawing.Size(81, 21);
+            this.btnFilterOk.TabIndex = 14;
+            this.btnFilterOk.Text = "Фільтр";
+            this.btnFilterOk.UseVisualStyleBackColor = true;
+            this.btnFilterOk.Click += new System.EventHandler(this.btnFilterOk_Click);
+            // 
+            // cmbSex
+            // 
+            this.cmbSex.FormattingEnabled = true;
+            this.cmbSex.Location = new System.Drawing.Point(584, 108);
+            this.cmbSex.Name = "cmbSex";
+            this.cmbSex.Size = new System.Drawing.Size(121, 21);
+            this.cmbSex.TabIndex = 13;
+            // 
+            // cmbAdres
+            // 
+            this.cmbAdres.FormattingEnabled = true;
+            this.cmbAdres.Location = new System.Drawing.Point(584, 60);
+            this.cmbAdres.Name = "cmbAdres";
+            this.cmbAdres.Size = new System.Drawing.Size(121, 21);
+            this.cmbAdres.TabIndex = 12;
+            // 
+            // txtReitTo
+            // 
+            this.txtReitTo.Location = new System.Drawing.Point(203, 109);
+            this.txtReitTo.Name = "txtReitTo";
+            this.txtReitTo.Size = new System.Drawing.Size(100, 20);
+            this.txtReitTo.TabIndex = 11;
+            // 
+            // txtReitFrom
+            // 
+            this.txtReitFrom.Location = new System.Drawing.Point(203, 58);
+            this.txtReitFrom.Name = "txtReitFrom";
+            this.txtReitFrom.Size = new System.Drawing.Size(100, 20);
+            this.txtReitFrom.TabIndex = 10;
+            // 
+            // txtPIP
+            // 
+            this.txtPIP.Location = new System.Drawing.Point(17, 84);
+            this.txtPIP.Name = "txtPIP";
+            this.txtPIP.Size = new System.Drawing.Size(131, 20);
+            this.txtPIP.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(623, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 15);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Стать";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(618, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 15);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Адреса";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(310, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 15);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "До:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(308, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Від:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(430, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Дата народження";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(163, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "До:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(163, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Від:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(222, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Рейтинг";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(48, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Прізвище:";
+            // 
             // frmOsoba
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(886, 465);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtFind);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.bindingNavigator1);
@@ -200,6 +423,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
             this.bindingNavigator1.ResumeLayout(false);
             this.bindingNavigator1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,5 +446,25 @@
         private System.Windows.Forms.ToolStripButton btnFind;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.ToolStripButton btnFilter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbSex;
+        private System.Windows.Forms.ComboBox cmbAdres;
+        private System.Windows.Forms.TextBox txtReitTo;
+        private System.Windows.Forms.TextBox txtReitFrom;
+        private System.Windows.Forms.TextBox txtPIP;
+        private System.Windows.Forms.DateTimePicker dtpDNOTo;
+        private System.Windows.Forms.DateTimePicker dtpDNOFrom;
+        private System.Windows.Forms.Button btnFilterCancel;
+        private System.Windows.Forms.Button btnFilterOk;
     }
 }
--- Main/frmOsoba.cs
+++ Main/frmOsoba.cs
@@ -20,24 +20,48 @@ namespace Main
 
         private void frmOsoba_Load(object sender, EventArgs e)
         {
+            this.Height = 320;
             h.bs1 = new BindingSource();
-            h.bs1.DataSource = h.myfunDt("Select * from information");
+            h.bs1.DataSource = h.myfunDt("Select * from info");
             dataGridView1.DataSource = h.bs1;
             bindingNavigator1.BindingSource = h.bs1;
+            dataGridView1.RowHeadersVisible = false;
 
-            h.bs1.Sort="adress,TN";
+
 
             DWGFormat();
+
+            DataTable dtBorder = new DataTable();
+            DataTable dtDistinct = new DataTable();
+            dtBorder = h.myfunDt("SELECT min(rating), max(rating), min(DNO), max(DNO) FROM info");
+            dtDistinct = h.myfunDt("SELECT distinct Adresa from info");
+
+            txtReitFrom.Text = dtBorder.Rows[0][0].ToString();
+            txtReitTo.Text = dtBorder.Rows[0][1].ToString();
+            dtpDNOFrom.Value = Convert.ToDateTime(dtBorder.Rows[0][2].ToString());
+            dtpDNOTo.Value = Convert.ToDateTime(dtBorder.Rows[0][3].ToString());
+
+            cmbAdres.Items.Add("");
+            for (int i = 0; i < dtDistinct.Rows.Count; i++)
+            {
+                cmbAdres.Items.Add(dtDistinct.Rows[i][0].ToString());
+            }
+            cmbAdres.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cmbSex.Items.Add("");
+            cmbSex.Items.Add("ч");
+            cmbSex.Items.Add("ж");
+            cmbSex.Text = "ч";
+            cmbSex.DropDownStyle = ComboBoxStyle.DropDownList;
         }
         void DWGFormat()
         {
             dataGridView1.Columns[0].Width = 25;
-            dataGridView1.Columns[1].HeaderText = "N";
-
         }
 
         private void btnFind_Click(object sender, EventArgs e)
         {
+
             if (btnFind.Checked)
             {
                 label1.Visible = true;
@@ -85,6 +109,78 @@ namespace Main
             }
         }
 
+        private void groupBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gfx = e.Graphics;
+            Pen p = new Pen(Color.DarkViolet, 1); // колір і товщина рамки
+            gfx.DrawLine(p, 0, 5, 5, 5);          // верхня горизонтальна лінія до Text
+            gfx.DrawLine(p, 35, 5, e.ClipRectangle.Width - 2, 5); // верхня горизонтальна лінія після Text
+            gfx.DrawLine(p, 0, 5, 0, e.ClipRectangle.Height - 2); // ліва вертикаль
+            gfx.DrawLine(p, e.ClipRectangle.Width - 2, 5, e.ClipRectangle.Width - 2, e.ClipRectangle.Height - 2); // права вертикаль
+            gfx.DrawLine(p, 0, e.ClipRectangle.Height - 2, e.ClipRectangle.Width - 2, e.ClipRectangle.Height - 2); // низ
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            if (btnFind.Enabled)
+            {
+                this.Height = 500;
+                groupBox1.Visible = true;
+            }
+            else
+            {
+                this.Height = 320;
+                groupBox1.Visible = false;
+            }
+        }
+
+        private void btnFilterOk_Click(object sender, EventArgs e)
+        {
+            string strFilter = "ID > 0";
+
+            if (!string.IsNullOrWhiteSpace(txtPIP.Text))
+            {
+                strFilter += $" AND PIPO LIKE '{txtPIP.Text}%'";
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtReitFrom.Text) && !string.IsNullOrWhiteSpace(txtReitTo.Text))
+            {
+                strFilter += $" AND (Rating >= {txtReitFrom.Text.Replace(',', '.')} AND Rating <= {txtReitTo.Text.Replace(',', '.')})";
+            }
+            else if (string.IsNullOrWhiteSpace(txtReitFrom.Text) && !string.IsNullOrWhiteSpace(txtReitTo.Text))
+            {
+                strFilter += $" AND (Rating <= {txtReitTo.Text.Replace(',', '.')})";
+            }
+            else if (!string.IsNullOrWhiteSpace(txtReitFrom.Text) && string.IsNullOrWhiteSpace(txtReitTo.Text))
+            {
+                strFilter += $" AND (Rating >= {txtReitFrom.Text.Replace(',', '.')})";
+            }
+
+            strFilter += $" AND (DNO >= #{dtpDNOFrom.Value:yyyy-MM-dd}# AND DNO <= #{dtpDNOTo.Value:yyyy-MM-dd}#)";
+
+            if (!string.IsNullOrWhiteSpace(cmbAdres.Text))
+            {
+                strFilter += $" AND (Adresa = '{cmbAdres.Text}')";
+            }
+
+
+            if (cmbSex.Text == "ч")
+            {
+                strFilter += " AND (Sex = 0)";
+            }
+            else if (cmbSex.Text == "ж")
+            {
+                strFilter += " AND (Sex = 1)";
+            }
+
+            h.bs1.Filter = strFilter;
+
+        }
+
+        private void btnFilterCancel_Click(object sender, EventArgs e)
+        {
+            h.bs1.RemoveFilter();
+        }
 
     }
 }
